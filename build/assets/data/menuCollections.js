0951cfa3a8c5396436dca6585205bf80c6680f8e
dandeeee <dandeeee@ya.ru>
2015-08-05 10:55:03 +0300

    rebuild 'build' directory

---
+++ build/assets/data/menuCollections.js
@@ -0,0 +1,113 @@
+COLLECTIONS =
+    [
+        {
+            title : "Caravaggio Pearl / Red Pearl",
+            url : "/collections/caravaggio-pearl.html",
+            thumbnail : "/assets/img/collections/thumbnails/caravaggio-pearl.jpg"
+        },
+        {
+            title : "Zephyro Pearl / Red Pearl",
+            url : "/collections/zephyro-pearl.html",
+            thumbnail : "/assets/img/collections/thumbnails/zephyro-pearl.jpg"
+        },
+        {
+            title : "Krystalia",
+            url : "/collections/krystalia.html",
+            thumbnail : "/assets/img/collections/thumbnails/krystalia.jpg"
+        },
+        {
+            title : "Zephyro Silver/Gold",
+            url : "/collections/zephyro-silver-gold.html",
+            thumbnail : "/assets/img/collections/thumbnails/zephyro.jpg"
+        },
+        {
+            title : "Arthe Metal Silver/Gold",
+            url : "/collections/arthe-metal-silver-gold.html",
+            thumbnail : "/assets/img/collections/thumbnails/metal.jpg"
+        },
+        {
+            title : "Caravaggio Silver (Neutro)/Gold",
+            url : "/collections/caravaggio-silver-neutro-gold.html",
+            thumbnail : "/assets/img/collections/thumbnails/caravaggio.jpg"
+        },
+        {
+            title : "Nahir Rigato",
+            url : "/collections/nahir-rigato.html",
+            thumbnail : "/assets/img/collections/thumbnails/nahir-rigato.jpg"
+        },
+        {
+            title : "Nahir Broccato",
+            url : "/collections/nahir-broccato.html",
+            thumbnail : "/assets/img/collections/thumbnails/nahir-broccato.jpg"
+        },
+        {
+            title : "Nahir Craklè",
+            url : "/collections/nahir-crakle.html",
+            thumbnail : "/assets/img/collections/thumbnails/nahir-crackle.jpg"
+        },
+        {
+            title : "Travertino New Concept",
+            url : "/collections/travertino-new-concept.html",
+            thumbnail : "/assets/img/collections/thumbnails/travertino.jpg"
+        },
+        {
+            title : "Stuccomarmo",
+            url : "/collections/stuccomarmo.html",
+            thumbnail : "/assets/img/collections/thumbnails/stuccomarmo.jpg"
+        },
+        {
+            title : "Encausto fiorentino",
+            url : "/collections/encausto-fiorentino.html",
+            thumbnail : "/assets/img/collections/thumbnails/encausto.jpg"
+        },
+        {
+            title : "Cocciopesto",
+            url : "/collections/cocciopesto.html",
+            thumbnail : "/assets/img/collections/thumbnails/cocciopesto.jpg"
+        },
+        {
+            title : "Decorà",
+            url : "/collections/decora.html",
+            thumbnail : "/assets/img/collections/thumbnails/decora.jpg"
+        },
+        {
+            title : "Decorà Gold",
+            url : "/collections/decora-gold.html",
+            thumbnail : "/assets/img/collections/thumbnails/decora-gold.jpg"
+        },
+        {
+            title : "Decorà DeLuxe",
+            url : "/collections/decora-deluxe.html",
+            thumbnail : "/assets/img/collections/thumbnails/decora-deluxe.jpg"
+        },
+        {
+            title : "Velatura Classic",
+            url : "/collections/velatura-classic.html",
+            thumbnail : "/assets/img/collections/thumbnails/velatura-classic.jpg"
+        },
+        {
+            title : "Velatura Rustica",
+            url : "/collections/velatura-rustica.html",
+            thumbnail : "/assets/img/collections/thumbnails/velatura-rustica.jpg"
+        },
+        {
+            title : "Ceralux",
+            url : "/collections/ceralux.html",
+            thumbnail : "/assets/img/collections/thumbnails/ceralux.jpg"
+        },
+        {
+            title : "Ardore",
+            url : "/collections/ardore.html",
+            thumbnail : "/assets/img/collections/thumbnails/ardore.jpg"
+        },
+        {
+            title : "Nobilis",
+            url : "/collections/nobilis.html",
+            thumbnail : "/assets/img/collections/thumbnails/nobilis.jpg"
+        },
+        {
+            title : "Rust Style",
+            url : "/collections/rust-style.html",
+            thumbnail : "/assets/img/collections/thumbnails/rust-style.jpg"
+        }
+    ];
