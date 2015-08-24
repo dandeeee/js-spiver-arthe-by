1c56fb2336af4fc8dbb4b2a5a9a2a37b8b62f0a3
daniel <Daniel.Dedkov@cib.de>
2015-08-24 13:05:06 +0300

    Add more info sections

--- build/assets/data/menuDop.js
+++ build/assets/data/menuDop.js
@@ -4,65 +4,18 @@ DOP =
 {
     title : "Краски для помещений",
     url : "/dop/vnutrenniye-kraski/"
-    //sub :
-    //[
-    //    {
-    //        title : "Краски против плесени",
-    //        url : "/dop/stenovye-kraski/protiv-pleseni.html"
-    //    },
-    //    {
-    //        title : "Акриловые краски",
-    //        url : "/dop/stenovye-kraski/acryl.html"
-    //    },
-    //    {
-    //        title : "Экстра моющиеся",
-    //        url : "/dop/stenovye-kraski/extra-moyushiesya.html"
-    //    },
-    //    {
-    //        title : "Моющиеся",
-    //        url : "/dop/stenovye-kraski/moyushiesya.html"
-    //    },
-    //    {
-    //        title : "Дышащие",
-    //        url : "/dop/stenovye-kraski/dyshashchie.html"
-    //    }
-    //]
-},
-{
-    title : "Фасадные краски",
-    url : "/dop/fasadnye-kraski/"
-    //sub :
-    //[
-    //    {
-    //        title : "Идеальная коллекция",
-    //        url : "/dop/fasadnyye-kraski/idealnaya-kollektsiya.html"
-    //    },
-    //    {
-    //        title : "Коллекция Спивер",
-    //        url : "/dop/fasadnyye-kraski/kollektsiya-spiver.html"
-    //    },
-    //    {
-    //        title : "Спивер кварцевая колеекция",
-    //        url : "/dop/fasadnyye-kraski/spiver-kvartsevaya-koleektsiya.html"
-    //    },
-    //    {
-    //        title : "Синтетические стеновые покрытия",
-    //        url : "/dop/fasadnyye-kraski/sinteticheskie-stenovye-pokrytiya.html"
-    //    },
-    //    {
-    //        title : "Минеральные стеновые покрытия",
-    //        url : "/dop/fasadnyye-kraski/mineralnye-stenovye-pokrytiya.html"
-    //    }
-    //]
 },
 {
     title : "Грунты",
     url : "/dop/grunty/"
 },
-
 {
     title : "Эмали",
     url : "/dop/emali/"
+},
+{
+    title : "Фасадные покрытия",
+    url : "/dop/fasadnye-pokrytiya/"
 },
 {
     title : "Пропитки и защитные средства",
@@ -83,25 +36,6 @@ DOP =
 {
 	title : "Специальные продукты",
     url : "/dop/specialnye-producty/"
-    //sub :
-	//[
-	//	{
-	//		title : "Капсулы из асбеста и цемента",
-	//			url : "/dop/spec-prod/kapsuly-iz-asbesta-tsementa.html"
-	//	},
-	//	{
-	//		title : "Специальные гидроизолирующие продукты",
-	//			url : "/dop/spec-prod/spetsialnye-gidroizoliruyushchie-produkty.html"
-	//	},
-	//	{
-	//		title : "Нетоксичные краски для дерева",
-	//			url : "/dop/spec-prod/netoksichnye-kraski-dlya-dereva.html"
-	//	},
-	//	{
-	//		title : "Барьерные материалы против вздутия",
-	//			url : "/dop/spec-prod/barjernye-materialy-protiv-vzdutiya.html"
-	//	}
-	//]
 },
 {
     title : "Тонирующие системы",
